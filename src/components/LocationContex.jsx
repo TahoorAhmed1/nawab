b759b53498c8ed6f684e4e8f21170b10dfd11239
TahoorAhmed <ahmedtahoor5@gmail.com>
2024-12-22 21:50:09 +0500

    done

--- src/components/LocationContex.jsx
+++ src/components/LocationContex.jsx
@@ -3,7 +3,7 @@ import { useState, useContext, createContext } from "react";
 const LocationContext = createContext();
 
 export const LocationProvider = ({ children }) => {
-  const [selectedLocation, setSelectedLocation] = useState("Toronto, Ontario");
+  const [selectedLocation, setSelectedLocation] = useState("");
 
   return (
     <LocationContext.Provider value={{ selectedLocation, setSelectedLocation }}>
