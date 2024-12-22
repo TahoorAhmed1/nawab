3a6cf08df09aed1bdb019de6003490403f67c1fc
TahoorAhmed <ahmedtahoor5@gmail.com>
2024-12-22 15:17:57 +0500

    done

--- src/components/home/buycar.jsx
+++ src/components/home/buycar.jsx
@@ -15,31 +15,31 @@ export default function SellCar() {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         {/* Dealer Ship */}
-        <div className=" shadow-lg  h-[396.88px]  rounded-sm">
+        <div className=" shadow-md  h-[396.88px]  rounded-sm">
           <p className="text-3xl py-6 bg-[#E5E3DF] font-semibold text-[#666666] text-center ">Dealer Ship</p>
           <div className=" flex flex-col gap-y-[16px] py-[50px] px-[23px]">
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Payment can take 20 days</span>
+              <span className="text-[#0000008F]">Higher Prices</span>
             </div>
             
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Lengthy inspection</span>
+              <span className="text-[#0000008F]">Limited Inventory</span>
             </div>
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Requires vehicle prep</span>
+              <span className="text-[#0000008F]">No inspection report</span>
             </div>
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Be ready to wait</span>
+              <span className="text-[#0000008F]">Mostly former rental cars</span>
             </div>
           </div>
         </div>
 
         {/* Soni Auto Market */}
-        <div className=" shadow-lg  h-[559px]  rounded-sm">
+        <div className=" shadow-xl  h-[559px]  rounded-sm">
             <div className="py-6  bg-[#F40000] flex justify-center ">
 
           <p className="text-3xl  font-semibold text-white text-center ">Soni Auto Market</p>
@@ -47,56 +47,56 @@ export default function SellCar() {
           <div className=" flex flex-col gap-y-[16px] py-[50px] px-[23px]">
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Payment can take 20 days</span>
+              <span className="text-black">Pay less then marketplace</span>
             </div>
             
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Lengthy inspection</span>
+              <span className="text-black">Pre book cars of your choice cars</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Requires vehicle prep</span>
+              <span className="text-black">Rv boats</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Be ready to wait</span>
+              <span className="text-black">Hundreds of cars to pick from</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Be ready to wait</span>
+              <span className="text-black">Certified inspection report</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Be ready to wait</span>
+              <span className="text-black">No former rental cars or texis</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Be ready to wait</span>
+              <span className="text-black">No haggling</span>
             </div>
           </div>
         </div>
 
        {/* Dealer Ship */}
-       <div className=" shadow-lg  h-[396.88px]  rounded-sm">
+       <div className=" shadow-md  h-[396.88px]  rounded-sm">
           <p className="text-3xl py-6 bg-[#E5E3DF]  font-semibold text-[#666666] text-center ">Dealer Ship</p>
           <div className=" flex flex-col gap-y-[16px] py-[50px] px-[23px]">
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Payment can take 20 days</span>
+              <span className="text-[#0000008F]">Haggle with Strangers</span>
             </div>
             
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Lengthy inspection</span>
+              <span className="text-[#0000008F]">Potentially stolen car</span>
             </div>
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Requires vehicle prep</span>
+              <span className="text-[#0000008F]">No inspection report</span>
             </div>
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Be ready to wait</span>
+              <span className="text-[#0000008F]">Unclear vehicle history</span>
             </div>
           </div>
         </div>
@@ -110,7 +110,7 @@ export default function SellCar() {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         {/* Dealer Ship */}
-        <div className=" shadow-lg  h-[396.88px]  rounded-sm">
+        <div className=" shadow-md  h-[396.88px]  rounded-sm">
           <p className="text-3xl py-6 bg-[#E5E3DF] font-semibold text-[#666666] text-center ">Dealer Ship</p>
           <div className=" flex flex-col gap-y-[16px] py-[50px] px-[23px]">
             <div className="flex items-start gap-3">
@@ -118,6 +118,10 @@ export default function SellCar() {
               <span className="text-[#0000008F]">Payment can take 20 days</span>
             </div>
             
+            <div className="flex items-start gap-3">
+              <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
+              <span className="text-[#0000008F]">Haggling guaranteed</span>
+            </div>
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
               <span className="text-[#0000008F]">Lengthy inspection</span>
@@ -134,7 +138,7 @@ export default function SellCar() {
         </div>
 
         {/* Soni Auto Market */}
-        <div className=" shadow-lg  h-[507px]  rounded-sm">
+        <div className=" shadow-xl  h-[507px]  rounded-sm">
             <div className="py-6  bg-[#F40000] flex justify-center ">
 
           <p className="text-3xl  font-semibold text-white text-center ">Soni Auto Market</p>
@@ -144,27 +148,27 @@ export default function SellCar() {
             
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Lengthy inspection</span>
+              <span className="text-black">We pay instantly</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Requires vehicle prep</span>
+              <span className="text-black">We don’t haggle</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Be ready to wait</span>
+              <span className="text-black">Our offers are firm</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Be ready to wait</span>
+              <span className="text-black">We’ll buy your car today</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Be ready to wait</span>
+              <span className="text-black">No need to clean your car</span>
             </div>
             <div className="flex items-start gap-3">
               <LuSmilePlus className="w-8 h-8 flex-shrink-0 text-[#0D9E11]" />
-              <span className="text-black">Be ready to wait</span>
+              <span className="text-black">We can pick up your car</span>
             </div>
           </div>
         </div>
@@ -175,12 +179,16 @@ export default function SellCar() {
           <div className=" flex flex-col gap-y-[16px] py-[50px] px-[23px]">
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Payment can take 20 days</span>
+              <span className="text-[#0000008F]">Insecure payment</span>
             </div>
             
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
-              <span className="text-[#0000008F]">Lengthy inspection</span>
+              <span className="text-[#0000008F]">Haggling guaranteed</span>
+            </div>
+            <div className="flex items-start gap-3">
+              <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
+              <span className="text-[#0000008F]">No-shows</span>
             </div>
             <div className="flex items-start gap-3">
               <FiFrown className="w-8 h-8 flex-shrink-0 text-black" />
