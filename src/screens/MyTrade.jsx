b037036303947de16ec89d00c98087168700b3fe
TahoorAhmed <ahmedtahoor5@gmail.com>
2024-12-22 15:25:08 +0500

    fix

--- src/screens/MyTrade.jsx
+++ src/screens/MyTrade.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./MyTrade.css";
 import Footer from "../components/Footer";
+// 
 import Swal from "sweetalert2";
 
 const MyTrade = () => {
